52b57840c1846fd2424a017fc6b3f0cb7139bcde
Yes_Im <50667072+adideas@users.noreply.github.com>
2023-05-29 01:44:02 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,17 +8,24 @@ public class Main {
     public static void main(String[] args) {
         var DH = new DiffieHellman();
 
-        BigInteger bobPrivateNumber = BigInteger.valueOf(RAND(10, 1000)); // rand bob
-        BigInteger bobKey = POW(DH.getGenerator(), bobPrivateNumber, DH.getPrimeNumber()); // send alice - get bob
-
-        BigInteger alicePublicKey = DH.getPrivateKey(bobKey); // not send
-      
-        BigInteger bobPublicKey = POW(DH.getPublicKey(), bobPrivateNumber, DH.getPrimeNumber());
-
-
-        System.out.println("alicePublicKey = " + alicePublicKey + " bobPublicKey = " + bobPublicKey);
+        // NOT SEND
+        BigInteger bobSimpleNumber = BigInteger.valueOf(RAND(10, 1000)); // rand bob
         
-        if (!(alicePublicKey.equals(bobPublicKey))) {
+        // send alice getGenerator and getPrimeNumber
+        BigInteger bobPublicKey = POW(DH.getGenerator(), bobSimpleNumber, DH.getPrimeNumber()); 
+        
+        // NOT SEND - alicePrivateKey
+        // send bob bobPublicKey
+        BigInteger alicePrivateKey = DH.getPrivateKey(bobPublicKey); 
+      
+        // NOT SEND - bobPrivateKey
+        BigInteger bobPrivateKey = POW(DH.getPublicKey(), bobPrivateNumber, DH.getPrimeNumber()); // not send
+
+
+        // CHECK
+        System.out.println("alicePrivateKey = " + alicePrivateKey + " bobPrivateKey = " + bobPrivateKey);
+        
+        if (!(alicePrivateKey.equals(bobPrivateKey))) {
             System.err.println("INVALID");
         }
     }
